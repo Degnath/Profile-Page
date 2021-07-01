40e67ed7cd1419f4d6cd31df473842fe7400cb1a
Deg Adhikari <dnadhikari@yahoo.com>
2021-07-01 14:59:10 -0400

    add commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function changeProfile(element){
+    var element = document.getElementById("switchName");
+    element.innerText ="Deg";
+}
+
+function connectionChange(element){
+    var element= document.getElementById("connectionChange");
+    element.innerText -=1;
+}
+
+function hide(id) {
+    var e = document.getElementById(id);
+    e.remove();
+}
+
+
